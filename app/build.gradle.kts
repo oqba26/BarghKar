75cad829619f06fe0d10a46c1618a845b0198e76
oqba26 <oqba26@gmail.com>
2026-08-26 15:46:59 +0330

    fix: update version files missed in previous commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.oqba26.barghkar"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
